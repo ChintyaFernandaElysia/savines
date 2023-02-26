43f0c7ef1c86b75b0948a87d6ca78aab3fbe938a
AngelVanessa <82552606+AngelVanessa@users.noreply.github.com>
2023-02-26 08:37:49 +0700

    update web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\IncomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,16 +15,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //testttt angel
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//Income
+// Route::get('/income', [IncomeController::class, 'index']);
+// Route::get('/income/add', [IncomeController::class, 'add']);
+// Route::post('/income/store', [IncomeController::class, 'store']);
+// Route::get('/income/edit/{id}', [IncomeController::class, 'edit']);
+// Route::post('/income/update/{id}', [IncomeController::class, 'update']);
+// Route::get('/income/destroy/{id}', [IncomeController::class, 'destroy']);
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('register', 'register')->name('register');
+    Route::post('register', 'registerSimpan')->name('register.simpan');
+
+    Route::get('login', 'login')->name('login');
+    Route::post('login', 'loginAksi')->name('login.aksi');
+
+    Route::get('logout', 'logout')->middleware('auth')->name('logout');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Income
-Route::get('/income', [IncomeController::class, 'index']);
-Route::get('/income/add', [IncomeController::class, 'add']);
-Route::post('/income/store', [IncomeController::class, 'store']);
-Route::get('/income/edit/{id}', [IncomeController::class, 'edit']);
-Route::post('/income/update/{id}', [IncomeController::class, 'update']);
-Route::get('/income/destroy/{id}', [IncomeController::class, 'destroy']);
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
-
+Route::controller(IncomeController::class)->prefix('incomes')->group(function () {
+    Route::get('', 'index')->name('incomes');
+    Route::get('create', 'create')->name('incomes.create');
+    Route::post('store', 'store')->name('incomes.store.simpan');
+    Route::get('edit/{id}', 'edit')->name('incomes.edit');
+    Route::post('edit/{id}', 'update')->name('incomes.update.update');
+    Route::get('destroy/{id}', 'destroy')->name('incomes.destroy');
+});
