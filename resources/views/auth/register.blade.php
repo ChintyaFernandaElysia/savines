cf130e6103905a199602c368851aa2f9d2e9ed55
AngelVanessa <82552606+AngelVanessa@users.noreply.github.com>
2023-03-01 09:51:59 +0700

    hapus file edit dan hapus image di view login dan register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,6 +28,7 @@
       <div class="card-body p-0">
         <!-- Nested Row within Card Body -->
         <div class="row">
+
           <div class="col-lg-12">
             <div class="p-5">
               <div class="text-center">
